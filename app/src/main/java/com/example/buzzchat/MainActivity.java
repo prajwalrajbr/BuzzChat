c4c8a3cb53bc8c7fa3e941619696a3aefd655a4c
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2020-08-04 20:05:56 +0530

    All Set for Development

--- app/src/main/java/com/example/buzzchat/MainActivity.java
+++ app/src/main/java/com/example/buzzchat/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.buzzchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Home(View view) {
+        Intent i = new Intent(this,Home.class);
+        startActivity(i);
+    }
+
+    public void IgScreen(View view) {
+        Intent i = new Intent(this,IgScreenActivity.class);
+        startActivity(i);
+    }
+
+    public void Profile(View view) {
+        Intent i = new Intent(this,ProfileActivity.class);
+        startActivity(i);
+    }
+
+    public void chatScreen(View view) {
+        Intent i = new Intent(this,chatScreen.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/buzzchat/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.buzzchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
