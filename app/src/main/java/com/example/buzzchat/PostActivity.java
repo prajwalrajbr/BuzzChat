09ba25222faa17100b934e5efb98ab6f327d5f5c
Benaka <benaka.nagesh@gmail.com>
2020-08-06 20:06:39 +0530

    update

---
+++ app/src/main/java/com/example/buzzchat/PostActivity.java
@@ -0,0 +1,14 @@
+package com.example.buzzchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//hi
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
